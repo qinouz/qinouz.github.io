eb1c79b1a453d3c7fb8f7cad8c1b691eecfbf5cc
qinouz <50247570@qq.com>
2017-03-26 14:25:33 +0800

    1
    1

---
+++ xiecheng/js/main.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+	var swiper = new Swiper('.swiper-container', {
+		// pagination: '.swiper-pagination',
+		// paginationClickable: true,
+		loop : true,
+		autoplay : 3500,
+		paginationType : 'custom',
+	});
+});
